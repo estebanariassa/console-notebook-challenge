b633b258d9c26f1db8e14d2cd80ca2762692cb72
monoj <estebanariassa@gmail.com>
2025-02-18 12:10:06 -0500

    primer commit

--- app/notebook.py
+++ app/notebook.py
@@ -1,1 +1,15 @@
 # TODO: Agrega el código de las clases del modelo aquí. Borra este comentario al terminar.
+from datetime import datetime
+
+class Note:
+    HIGH: str = 'HIGH'
+    MEDIUM: str = 'MEDIUM'
+    LOW: str = 'LOW'
+
+    def __init__(self, code, title, text, importance, creation_date):
+
+        code: str = code
+        title: str = title
+        text: str = text
+        importance: str = importance
+        creation_date: datetime = creation_date
